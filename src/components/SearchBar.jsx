28d7f150a995871e916b5d6f8939f180d47b49ec
WalterJuniorBarreto <martin.barreto.com@gmail.com>
2025-11-30 16:30:24 -0500

    version 1

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -12,16 +12,14 @@ function SearchBar({ value, onChange }) {
     };
 
     return (
-        // 3. Ya no es un <form>, es solo el contenedor del input
         <div className={styles.searchWrapper}>
             <input 
                 type="text"
                 placeholder="Escribe una ciudad..."
-                value={value} // 4. El valor es controlado desde afuera
-                onChange={handleChange} // 5. Llama a la función del padre
+                value={value} 
+                onChange={handleChange} 
                 className={styles.searchInput}
             />
-            {/* 6. Eliminamos el botón de "Buscar", la búsqueda es automática */}
         </div>
     );
 }
